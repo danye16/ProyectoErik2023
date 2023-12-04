057ff3871994797159d7c505ed94a42abe326c3d
cono200 <tatattian04@gmail.com>
2023-12-04 14:32:28 -0700

    Rama que quiero usar por que ya puse los grid y los label

--- ProyectoErik2023/FormularioPilas.Designer.cs
+++ ProyectoErik2023/FormularioPilas.Designer.cs
@@ -34,15 +34,30 @@
             this.btn_imprimir_pila = new System.Windows.Forms.Button();
             this.btn_ascendente_pila = new System.Windows.Forms.Button();
             this.agregar_elementoP = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.memoriaRam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tarjetaVideo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SSD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RGB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMemoriaRam = new System.Windows.Forms.TextBox();
+            this.txtTarjetaVideo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSSD = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRGB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_descendente_pila
             // 
             this.btn_descendente_pila.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_descendente_pila.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_descendente_pila.Location = new System.Drawing.Point(606, 313);
+            this.btn_descendente_pila.Location = new System.Drawing.Point(808, 385);
+            this.btn_descendente_pila.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_descendente_pila.Name = "btn_descendente_pila";
-            this.btn_descendente_pila.Size = new System.Drawing.Size(163, 69);
+            this.btn_descendente_pila.Size = new System.Drawing.Size(217, 85);
             this.btn_descendente_pila.TabIndex = 17;
             this.btn_descendente_pila.Text = "Ordenar Descendente";
             this.btn_descendente_pila.UseVisualStyleBackColor = false;
@@ -51,9 +66,10 @@
             // 
             this.btn_buscar_elementoP.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_buscar_elementoP.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_buscar_elementoP.Location = new System.Drawing.Point(31, 327);
+            this.btn_buscar_elementoP.Location = new System.Drawing.Point(808, 168);
+            this.btn_buscar_elementoP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_buscar_elementoP.Name = "btn_buscar_elementoP";
-            this.btn_buscar_elementoP.Size = new System.Drawing.Size(163, 69);
+            this.btn_buscar_elementoP.Size = new System.Drawing.Size(217, 85);
             this.btn_buscar_elementoP.TabIndex = 16;
             this.btn_buscar_elementoP.Text = "Buscar Elemento";
             this.btn_buscar_elementoP.UseVisualStyleBackColor = false;
@@ -62,9 +78,10 @@
             // 
             this.limpia_pila.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.limpia_pila.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.limpia_pila.Location = new System.Drawing.Point(319, 114);
+            this.limpia_pila.Location = new System.Drawing.Point(808, 261);
+            this.limpia_pila.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.limpia_pila.Name = "limpia_pila";
-            this.limpia_pila.Size = new System.Drawing.Size(163, 69);
+            this.limpia_pila.Size = new System.Drawing.Size(217, 85);
             this.limpia_pila.TabIndex = 15;
             this.limpia_pila.Text = "Limpiar Pila";
             this.limpia_pila.UseVisualStyleBackColor = false;
@@ -73,9 +90,10 @@
             // 
             this.btn_imprimir_pila.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_imprimir_pila.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_imprimir_pila.Location = new System.Drawing.Point(606, 54);
+            this.btn_imprimir_pila.Location = new System.Drawing.Point(808, 66);
+            this.btn_imprimir_pila.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_imprimir_pila.Name = "btn_imprimir_pila";
-            this.btn_imprimir_pila.Size = new System.Drawing.Size(163, 69);
+            this.btn_imprimir_pila.Size = new System.Drawing.Size(217, 85);
             this.btn_imprimir_pila.TabIndex = 14;
             this.btn_imprimir_pila.Text = "Imprimir Pila";
             this.btn_imprimir_pila.UseVisualStyleBackColor = false;
@@ -84,9 +102,10 @@
             // 
             this.btn_ascendente_pila.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_ascendente_pila.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_ascendente_pila.Location = new System.Drawing.Point(319, 260);
+            this.btn_ascendente_pila.Location = new System.Drawing.Point(808, 478);
+            this.btn_ascendente_pila.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_ascendente_pila.Name = "btn_ascendente_pila";
-            this.btn_ascendente_pila.Size = new System.Drawing.Size(163, 69);
+            this.btn_ascendente_pila.Size = new System.Drawing.Size(217, 85);
             this.btn_ascendente_pila.TabIndex = 13;
             this.btn_ascendente_pila.Text = "Ordenar Ascendente";
             this.btn_ascendente_pila.UseVisualStyleBackColor = false;
@@ -95,27 +114,151 @@
             // 
             this.agregar_elementoP.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.agregar_elementoP.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.agregar_elementoP.Location = new System.Drawing.Point(31, 54);
+            this.agregar_elementoP.Location = new System.Drawing.Point(583, 81);
+            this.agregar_elementoP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.agregar_elementoP.Name = "agregar_elementoP";
-            this.agregar_elementoP.Size = new System.Drawing.Size(163, 69);
+            this.agregar_elementoP.Size = new System.Drawing.Size(217, 85);
             this.agregar_elementoP.TabIndex = 12;
             this.agregar_elementoP.Text = "Agregar Elemento";
             this.agregar_elementoP.UseVisualStyleBackColor = false;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.memoriaRam,
+            this.tarjetaVideo,
+            this.SSD,
+            this.RGB});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 355);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(608, 150);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // memoriaRam
+            // 
+            this.memoriaRam.HeaderText = "Memoria Ram";
+            this.memoriaRam.MinimumWidth = 6;
+            this.memoriaRam.Name = "memoriaRam";
+            this.memoriaRam.Width = 125;
+            // 
+            // tarjetaVideo
+            // 
+            this.tarjetaVideo.HeaderText = "Tarjeta de Video";
+            this.tarjetaVideo.MinimumWidth = 6;
+            this.tarjetaVideo.Name = "tarjetaVideo";
+            this.tarjetaVideo.Width = 125;
+            // 
+            // SSD
+            // 
+            this.SSD.HeaderText = "SSD";
+            this.SSD.MinimumWidth = 6;
+            this.SSD.Name = "SSD";
+            this.SSD.Width = 125;
+            // 
+            // RGB
+            // 
+            this.RGB.HeaderText = "RGB";
+            this.RGB.MinimumWidth = 6;
+            this.RGB.Name = "RGB";
+            this.RGB.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Memoria Ram";
+            // 
+            // txtMemoriaRam
+            // 
+            this.txtMemoriaRam.Location = new System.Drawing.Point(16, 81);
+            this.txtMemoriaRam.Name = "txtMemoriaRam";
+            this.txtMemoriaRam.Size = new System.Drawing.Size(100, 22);
+            this.txtMemoriaRam.TabIndex = 20;
+            // 
+            // txtTarjetaVideo
+            // 
+            this.txtTarjetaVideo.Location = new System.Drawing.Point(16, 197);
+            this.txtTarjetaVideo.Name = "txtTarjetaVideo";
+            this.txtTarjetaVideo.Size = new System.Drawing.Size(100, 22);
+            this.txtTarjetaVideo.TabIndex = 22;
+            this.txtTarjetaVideo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Tarjeta de Video";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtSSD
+            // 
+            this.txtSSD.Location = new System.Drawing.Point(16, 308);
+            this.txtSSD.Name = "txtSSD";
+            this.txtSSD.Size = new System.Drawing.Size(100, 22);
+            this.txtSSD.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "SSD";
+            // 
+            // txtRGB
+            // 
+            this.txtRGB.Location = new System.Drawing.Point(152, 308);
+            this.txtRGB.Name = "txtRGB";
+            this.txtRGB.Size = new System.Drawing.Size(100, 22);
+            this.txtRGB.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Luces Gaming ";
+            // 
             // FormularioPilas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtRGB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSSD);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTarjetaVideo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMemoriaRam);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_descendente_pila);
             this.Controls.Add(this.btn_buscar_elementoP);
             this.Controls.Add(this.limpia_pila);
             this.Controls.Add(this.btn_imprimir_pila);
             this.Controls.Add(this.btn_ascendente_pila);
             this.Controls.Add(this.agregar_elementoP);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormularioPilas";
             this.Text = "FormularioPilas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -127,5 +270,18 @@
         private System.Windows.Forms.Button btn_imprimir_pila;
         private System.Windows.Forms.Button btn_ascendente_pila;
         private System.Windows.Forms.Button agregar_elementoP;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn memoriaRam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tarjetaVideo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SSD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RGB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMemoriaRam;
+        private System.Windows.Forms.TextBox txtTarjetaVideo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSSD;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRGB;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ProyectoErik2023/FormularioPilas.cs
+++ ProyectoErik2023/FormularioPilas.cs
@@ -16,5 +16,15 @@ namespace ProyectoErik2023
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
